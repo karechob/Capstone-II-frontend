0b3ad5e44e1dcca277738e6d678e0fc0b0045bed
Kaifeng <1536771545kai@gmail.com>
2023-08-13 14:55:01 -0400

    setup impact metric feature

---
+++ capstone2/src/components/Impact.jsx
@@ -0,0 +1,14 @@
+import { React, useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+
+const Impact = () =>{
+
+    return(
+        <div>
+            Hi this is impact mertic
+        </div>
+    );
+}
+
+export default Impact;
