ef43c17bb5fed22a831dedaac7007f5a473b3c93
Kaifeng <1536771545kai@gmail.com>
2023-08-13 19:42:02 -0400

    setup backend endpoint request

--- capstone2/src/components/Impact.jsx
+++ capstone2/src/components/Impact.jsx
@@ -3,10 +3,22 @@ import { useLocation } from "react-router-dom";
 import axios from "axios";
 
 const Impact = () =>{
+    const [codebaseChanges,setCodebaseChanges] = useState();
 
+    async function fetchData(ownerOfRepo,repoName){
+        const result = await axios.post('http://localhost:8080/api/github/impact',{
+            owner: ownerOfRepo,
+            repo: repoName
+        })
+        setCodebaseChanges(result.data);
+    }
+    useEffect(() =>{
+        fetchData("karechob","Capstone-II-frontend");
+    },[])
     return(
         <div>
-            Hi this is impact mertic
+            <h1>Hi this is impact mertic</h1>Hi this is impact mertic
+            {codebaseChanges?(console.log(codebaseChanges)):(<h1>Loading.....</h1>)}
         </div>
     );
 }
