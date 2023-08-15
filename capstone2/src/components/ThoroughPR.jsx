f406aa768f8d720c0849f464a476cae695ae6c19
yuzchen7 <yuzchen7@gmail.com>
2023-08-15 15:36:52 -0400

    Revert "Merge remote-tracking branch 'refs/remotes/origin/main'"
    This reverts commit 923c0c79a5a140514385ee576431432f9773b4f2, reversing
    changes made to 109d47292c2edcaf9b8074666b20c4eb2a194a1c.

--- capstone2/src/components/ThoroughPR.jsx
+++ capstone2/src/components/ThoroughPR.jsx
@@ -1,49 +1,17 @@
 import { React, useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { Pie } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import axios from "axios";
 
-function ThoroughPR() {
-  ChartJS.register(ArcElement, Tooltip, Legend);
 
+function ThoroughPR() {
   const [owner, setOwner] = useState("");
   const [repo, setRepo] = useState("");
-  const [percentage, setPercentage] = useState("");
-  const [notThorough, setNotThorough] = useState("");
-  const authToken = process.env.REACT_APP_GITHUB_API_KEY;
+  const [percentage, setPercentage] = useState(""); 
+  const authToken = process.env.REACT_APP_GITHUB_API_KEY; 
 
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const link = searchParams.get("link");
-
-  const data = {
-    labels: ["ThoroughPRs", "Non-ThoroughPRs"],
-    datasets: [
-      {
-        label: "Percent",
-        data: [percentage, notThorough],
-        backgroundColor: [
-          'rgb(80, 200, 120)', //green
-          'rgb(222, 49, 99)', //red
-        ],
-        hoverBackgroundColor: [
-          'rgb(89, 224, 134)',
-          'rgb(247, 54, 109)'
-        ],
-        borderColor: [
-          'rgb(255,250,240)'
-        ],
-        borderWidth: [1],
-        hoverOffset: 4,
-      },
-    ],
-  };
-
-  const config = {
-    type: "pie",
-    data: data,
-  };
 
   useEffect(() => {
     function deconstructLink() {
@@ -53,101 +21,91 @@ function ThoroughPR() {
     }
 
     async function fetchPullRequests() {
-      try {
-        if (owner && repo) {
-          //console.log(owner);
-          //console.log(repo);
+        try {
+          if (owner && repo) {
+            //console.log(owner);
+            //console.log(repo);
 
-          let allPullRequests = [];
-          let page = 1;
-          let hasMorePullRequests = true;
-
-          while (hasMorePullRequests) {
-            const response = await axios.get(
-              `https://api.github.com/repos/${owner}/${repo}/pulls`,
-              {
-                headers: {
-                  Authorization: `Bearer ${authToken}`,
-                },
-                params: {
-                  state: "closed",
-                  base: "main",
-                  page: page,
-                  per_page: 100,
-                },
+            let allPullRequests = [];
+            let page = 1;
+            let hasMorePullRequests = true;
+      
+            while (hasMorePullRequests) {
+              const response = await axios.get(
+                `https://api.github.com/repos/${owner}/${repo}/pulls`,
+                {
+                  headers: {
+                    Authorization: `Bearer ${authToken}`,
+                  },
+                  params: {
+                    state: "closed",
+                    base: "main",
+                    page: page,
+                    per_page: 100,
+                  },
+                }
+              );
+      
+              const pullRequests = response.data;
+              allPullRequests = allPullRequests.concat(pullRequests);
+      
+              // Check if there are more pages
+              if (pullRequests.length === 0) {
+                hasMorePullRequests = false;
+              } else {
+                page++;
               }
-            );
-
-            const pullRequests = response.data;
-            allPullRequests = allPullRequests.concat(pullRequests);
-
-            // Check if there are more pages
-            if (pullRequests.length === 0) {
-              hasMorePullRequests = false;
-            } else {
-              page++;
             }
-          }
+      
+            let totalRequests = allPullRequests.length;
+            let thoroughRequests = 0;
+            console.log(totalRequests + " total");
+      
+            for (const request of allPullRequests) {
 
-          let totalRequests = allPullRequests.length;
-          let thoroughRequests = 0;
-          //console.log(totalRequests + " total");
-
-          for (const request of allPullRequests) {
-            const commentsResponse = await axios.get(request.comments_url, {
-              headers: {
-                Authorization: `Bearer ${authToken}`,
-              },
+              const commentsResponse = await axios.get(request.comments_url, {
+                headers:  {
+                    Authorization: `Bearer ${authToken}`,
+                }
             });
-
-            const comments = commentsResponse.data;
-
-            if (comments.length > 0) {
-              thoroughRequests++;
+              const comments = commentsResponse.data;
+      
+              if (comments.length > 0) {
+                thoroughRequests++;
+              }
             }
+      
+            //console.log(thoroughRequests + " thorough");
+            setPercentage(Math.round((thoroughRequests / totalRequests) * 100));
+            //console.log(percentage + "%");
           }
-
-          //console.log(thoroughRequests);
-          setPercentage(Math.round((thoroughRequests / totalRequests) * 100));
-          //console.log(percentage);
-          setNotThorough(Math.round(((totalRequests - thoroughRequests) / totalRequests) * 100));
-          //console.log(notThorough);
+        } catch (error) {
+          console.log("error fetching pull requests " + error);
         }
-      } catch (error) {
-        console.log("error fetching pull requests " + error);
       }
-    }
-
+      
     deconstructLink();
     fetchPullRequests();
   }, [link, owner, repo]);
 
   return (
     <div>
-      
-      {/* <h1>Thorough PRs</h1>
-      <h4>
-        Thoroughly reviewed PRS is the percentage of merged pull requests with
-        at least one comment. This metric helps you understand your team's code
-        review quality. The higher the percentage, the better off you and your
-        team are!
-        <br></br>
-        Please note our metric measures to the nearest whole number.
+      <h1>Thorough PRs</h1>
+      <h4>Thoroughly reviewed PRS is the percentage of merged pull requests with at least one comment.
+          This metric helps you understand your team's code review quality. The higher the percentage, 
+          the better off you and your team are! 
+          <br></br>
+          Please note our metric measures to the nearest whole number. 
       </h4>
       <p>Link: {link} </p>
       <p>Owner: {owner} </p>
-      <p>Repo: {repo} </p> */}
+      <p>Repo: {repo} </p>
 
-      {percentage === "" ? (
-        <p>Percentage of Thorough PRs: Calculating... One Moment...</p>
+      {percentage === "" ? ( 
+        <p>Percentage of Thorough PRs: Calculating...</p>
       ) : (
-        // <p>Percentage of Thorough PRs: {percentage}% </p>
-        <div style={{ width: "400px", height: "400px" }}>
-          <Pie data={data} options={config} />
-        </div>
+        <p>Percentage of Thorough PRs: {percentage}% </p>
       )}
-
-      
 
     </div>
   );
